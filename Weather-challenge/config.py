cf772cb845893b22f049e25c7ade8fe7710f0854
Mariana Chavez <50565467+marianiviris@users.noreply.github.com>
2019-07-11 19:12:36 -0500

    Add files via upload

---
+++ Weather-challenge/config.py
@@ -0,0 +1,4 @@
+api_key = "AKGcFfAgokCvoHoBLovS6r08VJZtDoeQ"
+gkey = "AIzaSyBm_epW2CscZKWCAG9qPkUaQbKiFLc93wU"
+census_key = "90a3b4413de14a5a52626999ba58589f7a37f90d"
+weather_key = "163ecdb60c77eb643f11300d70c681ba"
